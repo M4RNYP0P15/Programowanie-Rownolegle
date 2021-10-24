bd8731270fc0a7b7692f90d7f20a19866ece75e0
M4RNYP0P15 <38810840+M4RNYP0P15@users.noreply.github.com>
2021-10-24 15:23:53 +0200

    Add files via upload
    Zad 2

---
+++ LAB 3/Czasomierz.java
@@ -0,0 +1,29 @@
+public class Czasomierz  extends Thread{
+    public static void main(String[] args) {
+        Czasomierz cz = new Czasomierz();
+        cz.start();
+    }
+
+    @Override
+    public void run() {
+        int sec =0, min=0,hr=0;
+        while (true){
+            try{
+                Thread.sleep(1000);
+            }catch (Exception exception){}
+            sec++;
+            if(sec==60){
+                min++;
+                sec=0;
+                if(min==60){
+                    hr++;
+                    min=0;
+                    if (hr==24){
+                        hr=0;
+                    }
+                }
+            }
+            System.out.println("Czas:"+hr+":"+min+":"+sec);
+        }
+    }
+}
