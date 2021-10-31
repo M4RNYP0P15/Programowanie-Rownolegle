3768612286a41ea1478429960f2cdb955ba595c2
M4RNYP0P15 <38810840+M4RNYP0P15@users.noreply.github.com>
2021-10-31 20:14:01 +0100

    Update GrayScale.java

--- LAB 3/GrayScale.java
+++ LAB 3/GrayScale.java
@@ -35,8 +35,8 @@ public class GrayScale extends Thread{
         GrayScale o = new GrayScale();
         try {
             //odczyt pixeli obrazu w dwóch pętlach po kolumnach i wierszach
-            for(int i=1; i<height-1; i++){
-                for(int j=1; j<width-1; j++){
+            for(int i=0; i<height; i++){
+                for(int j=0; j<width; j++){
                     new GrayScale(i,j).start();
                 }
             }
